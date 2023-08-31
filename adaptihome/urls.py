2927fd3bc564bf3aa5e7823a86476c82906f046a
Igor Grosu <igrgro@gmail.com>
2023-08-31 14:28:01 +0300

    collected static

--- adaptihome/urls.py
+++ adaptihome/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .settings import STATIC_URL, STATICFILES_DIRS, MEDIA_URL, MEDIA_ROOT, LOCALES_URL, LOCALES_ROOT
+from .settings import STATIC_URL, STATICFILES_ROOT, MEDIA_URL, MEDIA_ROOT, LOCALES_URL, LOCALES_ROOT
 from django.urls import path, include
 from django.conf.urls.static import static
 
@@ -8,7 +8,7 @@ urlpatterns = [
 	path('api/', include('api.urls')),
 	path('auth/', include('rest_framework.urls')),
 	path('', include('frontend.urls'))
-] + static(STATIC_URL, document_root=STATICFILES_DIRS)  + static(MEDIA_URL, document_root=MEDIA_ROOT) + static(LOCALES_URL, document_root=LOCALES_ROOT)
+] + static(STATIC_URL, document_root=STATICFILES_ROOT)  + static(MEDIA_URL, document_root=MEDIA_ROOT) + static(LOCALES_URL, document_root=LOCALES_ROOT)
 
 handler404 = 'frontend.views.redirect_to_home'
 handler500 = 'frontend.views.redirect_to_home'
