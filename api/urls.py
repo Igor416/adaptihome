9774f92c8af88b240c6cc587c80742d6ff83bf2e
Igor Grosu <igrgro@gmail.com>
2023-09-12 22:22:30 +0300

    separeted listed views

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,12 @@ from . import views
 urlpatterns = [
     path('search/', views.SearchView.as_view()),
     path('mattress_category_prices/', views.MattressColectionsPriceView.as_view()),
-    path('products/<str:category>/', views.ListProductView.as_view()),
-    path('products/<str:category>/<str:filter>/', views.ListProductView.as_view()),
-    path('product/<str:category>/<str:name>/', views.RetrieveProductView.as_view()),
+    path('products/folding_bed/', views.FoldingBedView.as_view()),
+    path('products/bed/', views.BedView.as_view()),
+    path('products/mattress/', views.MattressView.as_view()),
+    path('products/table/', views.TableView.as_view()),
+    path('products/accessories/', views.AccessoriesView.as_view()),
+    path('products/puff/', views.PuffView.as_view()),
+    path('product/<str:category>/<str:name>/', views.ProductView.as_view()),
     path('order/<str:id>/', views.OrderView.as_view())
 ]
